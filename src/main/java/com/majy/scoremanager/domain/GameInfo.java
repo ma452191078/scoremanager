6d3e6832a36a0f73b9c77946146f24b82e93dffe
Code <majingyuanno.1@gmail.com>
2017-08-21 21:43:32 +0800

    增加评分规则

--- src/main/java/com/majy/scoremanager/domain/GameInfo.java
+++ src/main/java/com/majy/scoremanager/domain/GameInfo.java
@@ -1,9 +1,12 @@
 package com.majy.scoremanager.domain;
 
+import lombok.Data;
+
 /**
  * Created by majingyuan on 2017/5/28.
  * 比赛类
  */
+@Data
 public class GameInfo {
     private String gameId;
     private String gameName;
@@ -16,92 +19,11 @@ public class GameInfo {
     private String gameStatus;
     private String gameQr;
     private String gameUrl;
+    private String gameRole;
 
     public String getGameId() {
         return gameId;
     }
 
-    public void setGameId(String gameId) {
-        this.gameId = gameId;
-    }
 
-    public String getGameName() {
-        return gameName;
-    }
-
-    public void setGameName(String gameName) {
-        this.gameName = gameName;
-    }
-
-    public String getGameActive() {
-        return gameActive;
-    }
-
-    public void setGameActive(String gameActive) {
-        this.gameActive = gameActive;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getAddBy() {
-        return addBy;
-    }
-
-    public void setAddBy(String addBy) {
-        this.addBy = addBy;
-    }
-
-    public String getGameOwner() {
-        return gameOwner;
-    }
-
-    public void setGameOwner(String gameOwner) {
-        this.gameOwner = gameOwner;
-    }
-
-    public String getGameDeleted() {
-        return gameDeleted;
-    }
-
-    public void setGameDeleted(String gameDeleted) {
-        this.gameDeleted = gameDeleted;
-    }
-
-    public String getGameStatus() {
-        return gameStatus;
-    }
-
-    public void setGameStatus(String gameStatus) {
-        this.gameStatus = gameStatus;
-    }
-
-    public String getGameQr() {
-        return gameQr;
-    }
-
-    public void setGameQr(String gameQr) {
-        this.gameQr = gameQr;
-    }
-
-    public String getGameUrl() {
-        return gameUrl;
-    }
-
-    public void setGameUrl(String gameUrl) {
-        this.gameUrl = gameUrl;
-    }
 }
